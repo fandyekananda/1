6f984a036804b2b2bac846ad7f81c4e4aeb939df
fandyekananda <61262017+fandyekananda@users.noreply.github.com>
2020-02-20 12:51:11 +0700

    Create adstera.js

---
+++ adstera.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+    script.type = 'text/javascript';
+
+    script.src = 'https://www.gstatic.com/firebasejs/3.4.0/firebase.js';
+    document.body.appendChild(script);
